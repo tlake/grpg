129f47416dcab5ae3385c7f2377a226ed65d035f
Tanner Lake <tanner.lake@gmail.com>
2020-10-30 13:35:11 -0700

    get window to render

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # grpg
-A silly Golang experiment at making an RPG. It's like a JRPG, but in Go.
+A silly Golang experiment to make a 2D sprite-based video game. It's like a JRPG, but in Go.
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+const (
+	screenWidth  = 800
+	screenHeight = 600
+)
+
+func main() {
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		fmt.Println("initializing sdl:", err)
+		return
+	}
+
+	window, err := sdl.CreateWindow(
+		"GoRPG",
+		sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		screenWidth, screenHeight,
+		sdl.WINDOW_OPENGL,
+	)
+	if err != nil {
+		fmt.Println("initializing window:", err)
+		return
+	}
+	defer window.Destroy()
+
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		fmt.Println("initializing renderer:", err)
+		return
+	}
+	defer renderer.Destroy()
+
+	for {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				return
+			}
+		}
+		renderer.SetDrawColor(0, 0, 0, 255)
+		renderer.Clear()
+		renderer.Present()
+	}
+}
